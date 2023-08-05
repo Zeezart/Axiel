f280d93c07f823050e5bf57d32e095015138f5c7
Zeezart <azeezhazeezah010@gmail.com>
2023-08-05 20:06:00 +0100

    Edited navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './navbar.css'
-import { FaCamera, FaPhone } from 'react-icons/fa'
+import { FaCamera, FaPhone, FaComment, FaUser, FaUserFriends } from 'react-icons/fa'
 
 function Navbar(){
     return(
         <div className='navbar'>
             <FaCamera />
             <FaPhone />
+            <FaComment />
+            <FaUser />
+            <FaUserFriends />
         </div>
     )
 }
